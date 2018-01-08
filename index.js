9849dda6dfee12200411aad4161c53f124beb029
feed4rz <mrspanky@yandex.ru>
2018-01-08 17:10:23 +0300

    Signature validation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config.json')
 const express = require('express')
 const bodyparser = require('body-parser')
 const fs = require('fs')
+const crypto = require('crypto')
 const { spawn } = require('child_process')
 
 /* Express app */
@@ -12,9 +13,26 @@ const app = express()
 app.use(bodyparser.json())
 
 app.post('/webhook', (req, res) => {
-	console.log(req.body)
+  if(!req.body.repository) return res.json({ success: false, err: 'Invalid body' })
+  if(!config.repositories[req.body.repository.full_name]) return res.json({ success: false, err: 'Invalid repository' })
+  if(!req.headers['x-hub-signature']) return res.json({ success: false, err: 'No signature provided' })
 
-	res.json({ success: true })
+  console.log(req.body)
+
+  const sha1 = crypto
+    .createHmac('sha1', config.repositories[req.body.repository.full_name].secret)
+    .update(JSON.stringify(req.body))
+    .digest('hex')
+
+  const signature = `sha1=${sha1}`
+
+  console.log(`computed sha1: ${sha1}`)
+  console.log(`computed signature: ${signature}`)
+  console.log(`original signature: ${req.headers['x-hub-signature']}`)
+
+  if(req.headers['x-hub-signature'] != signature) return res.json({ success: false, err: 'Invalid signature' })
+
+  res.json({ success: true })
 })
 
 app.listen(config.port)
